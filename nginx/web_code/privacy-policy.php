e6cc8ecf730c7e6db173f333db41a6a3fdfd5ea1
Darren Stokes <darrenstokes2@hotmail.com>
2024-02-27 12:56:06 +0100

    Renamed page after changing from HTML to PHP

---
+++ nginx/web_code/privacy-policy.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Darren Stokes - Cookie Policy</title>
+
+        <!-- CSS -->
+        <link href="https://fonts.googleapis.com/css?family=Schoolbell&v1" rel="stylesheet">
+        <link href="stylesheets/default.css" rel="stylesheet">
+
+        <!-- Javascript -->
+        <script src="scripts/javascript.js"></script>
+    </head>
+    <body>
+        <?php
+            include('banner.html');
+        ?>
+
+    </body>
+</html>
