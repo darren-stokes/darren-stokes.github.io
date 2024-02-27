78b8275a1736814e03b63703f8cc153579bafc94
Darren Stokes <darrenstokes2@hotmail.com>
2024-02-28 00:00:59 +0100

    Renamed "init_page" function to "set_language" to better represent its responsibilty. Configured the dark mode to save its state to local storage to persist site wide and reloads

--- nginx/web_code/scripts/javascript.js
+++ nginx/web_code/scripts/javascript.js
@@ -8,29 +8,44 @@ function toggle_language(language) {
     });
 }
 
-// Detect lanuage on page load and set dark mode
-function init_page(){
+// Detect language on page load
+function set_language(){
     let user_lang = navigator.language || navigator.userLanguage;
     user_lang = user_lang.startsWith('es') ? 'es' : 'en' // Defaults to English
     document.getElementById('language-select').value = user_lang;
     toggle_language(user_lang);
-    toggle_dark_mode();
 }
-
-// Event listener for language selection change
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('language-select').addEventListener('change', function(){
-        toggle_language(this.value);
-    });
-    init_page();
-});
 
 // Toggle dark mode
 function toggle_dark_mode(){
-    document.body.classList.toggle('dark-mode')
+    var is_checked = document.body.classList.toggle('dark-mode')
+    // Save to localStorage
+    localStorage.setItem('dark_mode', is_checked ? 'enabled': 'disabled')
+}
+
+function apply_dark_mode(){
+    // Enable dark mode if set in local storage
+    if (localStorage.getItem('dark_mode') === 'enabled'){
+        document.body.classList.add('dark-mode');
+        document.querySelector('.switch input[type="checkbox"]').checked = true;
+    }
+    // if not set, remove the CSS
+    else {
+        document.body.classList.remove('dark-mode');
+        document.querySelector('.switch input[type="checkbox"]').checked = false;
+    }
 }
 
 // Hide cookie notice
 function hide_cookie_notice(){
     document.getElementById('cookie-notice').style.display = 'none';
 }
+
+// On page load, set the language and check the local storage whether dark mode is set or not
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('language-select').addEventListener('change', function(){
+        toggle_language(this.value);
+    });
+    set_language();
+    apply_dark_mode();
+});
