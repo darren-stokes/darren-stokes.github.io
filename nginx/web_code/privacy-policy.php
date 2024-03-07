6f61e5f470b1626839ea68b9cff2213a60ab29b9
Darren Stokes <darrenstokes2@hotmail.com>
2024-03-07 13:34:04 +0100

    Added placeholder for the Spanish text. Also added a copyright footer to show on all pages

--- nginx/web_code/privacy-policy.php
+++ nginx/web_code/privacy-policy.php
@@ -16,8 +16,60 @@
             include('banner.html');
         ?>
 
+        <div class="content">
+            <div class="lang lang-en">
+                <p class="section-heading">Introduction</p>
+                    <p class="content-text">Welcome to our website. We are committed to protecting your privacy and ensuring a seamless browsing experience for you. This Cookie Policy explains how we use cookies and similar technologies to enhance your user experience and protect your privacy.</p>
+                <p class="section-heading">What Are Cookies?</p>
+                    <p class="content-text">Cookies are small text files placed on your device to store data that can be recalled by a web server in the domain that placed the cookie. They are widely used to make websites work more efficiently, as well as to provide information to the site owners.</p>
+                <p class="section-heading">How and Why We Use Cookies</p>
+                    <p class="content-text">Our website uses functional cookies to remember your settings and preferences during and between visits, enabling us to provide you with a more personalized experience. Specifically, we use cookies for:
+                        <ul>
+                            <li><strong>Language Preferences:</strong> To remember your language selection so our website can be displayed in your preferred language during subsequent visits.</li>
+                            <li><strong>Dark Mode Settings:</strong> To remember your choice of dark mode setting, ensuring that our website appears in your preferred theme consistently across all pages.</li>
+                            <li><strong>Browser Detection:</strong> To change the appearance of certain elements based on what browser is being used.</li>
+                        </ul>
+                    These cookies are strictly necessary for the website to function and cannot be switched off in our systems. They do not collect or store any personal information.</p>
+                <p class="section-heading">Consent and Control</p>
+                    <p class="content-text">By using our website, you consent to our use of cookies as described in this policy. However, you can control and manage cookies through your web browser settings. Please be aware that removing or blocking cookies may impact your user experience and parts of our website may no longer be fully accessible.</p>
+                <p class="section-heading">Data Protection and Privacy</p>
+                    <p class="content-text">We take your privacy seriously and have implemented measures to protect your personal information. The functional cookies we use are designed to enhance your browsing experience without compromising your privacy. For more detailed information on how we handle your personal data, please visit our Privacy Policy.</p>
+                <p class="section-heading">User Rights Under GDPR</p>
+                    <p class="content-text">Under the General Data Protection Regulation (GDPR), you have rights over your personal data, including the right to access, correct, or delete your data, and the right to withdraw consent. If you wish to exercise these rights or have any questions about our cookie policy, please contact us at [contact information].</p>
+                <p class="section-heading">Changes to the Cookie Policy</p>
+                    <p class="content-text">We may update this cookie policy from time to time to reflect changes in our practices, technology, or legal requirements. We encourage you to review this policy periodically to stay informed about how we use cookies.</p>
+                <p class="section-heading">Effective Date</p>
+                    <p class="content-text">This policy is effective as of 7th of March 2024</p>
+            </div>
+            <div class="lang lang-es">
+                <p class="section-heading">Spanish Introduction</p>
+                    <p class="content-text">Welcome to our website. We are committed to protecting your privacy and ensuring a seamless browsing experience for you. This Cookie Policy explains how we use cookies and similar technologies to enhance your user experience and protect your privacy.</p>
+                <p class="section-heading">What Are Cookies?</p>
+                    <p class="content-text">Cookies are small text files placed on your device to store data that can be recalled by a web server in the domain that placed the cookie. They are widely used to make websites work more efficiently, as well as to provide information to the site owners.</p>
+                <p class="section-heading">How and Why We Use Cookies</p>
+                    <p class="content-text">Our website uses functional cookies to remember your settings and preferences during and between visits, enabling us to provide you with a more personalized experience. Specifically, we use cookies for:
+                        <ul>
+                            <li><strong>Language Preferences:</strong> To remember your language selection so our website can be displayed in your preferred language during subsequent visits.</li>
+                            <li><strong>Dark Mode Settings:</strong> To remember your choice of dark mode setting, ensuring that our website appears in your preferred theme consistently across all pages.</li>
+                            <li><strong>Browser Detection:</strong> To change the appearance of certain elements based on what browser is being used.</li>
+                        </ul>
+                    These cookies are strictly necessary for the website to function and cannot be switched off in our systems. They do not collect or store any personal information.</p>
+                <p class="section-heading">Consent and Control</p>
+                    <p class="content-text">By using our website, you consent to our use of cookies as described in this policy. However, you can control and manage cookies through your web browser settings. Please be aware that removing or blocking cookies may impact your user experience and parts of our website may no longer be fully accessible.</p>
+                <p class="section-heading">Data Protection and Privacy</p>
+                    <p class="content-text">We take your privacy seriously and have implemented measures to protect your personal information. The functional cookies we use are designed to enhance your browsing experience without compromising your privacy. For more detailed information on how we handle your personal data, please visit our Privacy Policy.</p>
+                <p class="section-heading">User Rights Under GDPR</p>
+                    <p class="content-text">Under the General Data Protection Regulation (GDPR), you have rights over your personal data, including the right to access, correct, or delete your data, and the right to withdraw consent. If you wish to exercise these rights or have any questions about our cookie policy, please contact us at [contact information].</p>
+                <p class="section-heading">Changes to the Cookie Policy</p>
+                    <p class="content-text">We may update this cookie policy from time to time to reflect changes in our practices, technology, or legal requirements. We encourage you to review this policy periodically to stay informed about how we use cookies.</p>
+                <p class="section-heading">Effective Date</p>
+                    <p class="content-text">This policy is effective as of 7th of March 2024</p>
+            </div>
+        </div>
         <?php
-            include('cookie_notice.html')
+            include('footer.php');
+            echo "\n";
+            include('cookie_notice.html');
         ?>
     </body>
 </html>
