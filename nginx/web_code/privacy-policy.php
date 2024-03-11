b4b2c0a9c451df08467ac329e586e451dbc0b64e
Darren Stokes <darrenstokes2@hotmail.com>
2024-03-11 16:03:15 +0100

    Added the viewport meta tag so that mobile devices will show the page as intended

--- nginx/web_code/privacy-policy.php
+++ nginx/web_code/privacy-policy.php
@@ -2,7 +2,8 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Darren Stokes - Cookie Policy</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Darren Stokes - Privacy Policy</title>
 
         <!-- CSS -->
         <link href="https://fonts.googleapis.com/css?family=Schoolbell&v1" rel="stylesheet">
