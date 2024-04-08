72468e152be660d915d8fe68e3d76359b6028117
Darren Stokes <darrenstokes2@hotmail.com>
2024-04-08 17:05:47 +0200

    As GitHub Pages only supports static pages, remove all of the PHP elements

--- nginx/web_code/500s_page.php
+++ nginx/web_code/500s_page.php
@@ -1,15 +1,94 @@
 <!DOCTYPE html>
     <head>
         <title>Darren Stokes | 500 Error</title>
-        <?php
-            include('styles_and_scripts.html');
-        ?>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Tell Dark Reader that it doesn't need to apply as we have a dark mode defined -->
+        <meta name="darkreader-lock">
+
+        <!-- CSS -->
+        <link href="https://fonts.googleapis.com/css?family=Schoolbell&v1" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href="/stylesheets/default.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css" rel="stylesheet">
+
+        <!-- Javascript -->
+        <script src="/scripts/javascript.js"></script>
     </head>
     <body>
-        <?php
-        include('banner.php');
-        ?>
-        <div class="content">
+        <!-- Banner -->
+        <div class="navbar banner">
+            <div class="navbar-left">
+                <a href="/">Darren Stokes</a>
+            </div>
+            <!-- Hamburger Icon with transform for mobile devices. Credit: https://codepen.io/ainalem/pen/PoYvggj -->
+            <div class="mobile-nav-icon" onclick="toggleMobileMenu()">
+              <svg xmlns="http://www.w3.org/2000/svg" width="53.33" height="53.33" viewBox="0 0 200 200">
+                <g stroke-width="6.5" stroke-linecap="round">
+                  <path
+                    d="M72 82.286h28.75"
+                    fill="#009100"
+                    fill-rule="evenodd"
+                    stroke="#fff"
+                  />
+                  <path
+                    d="M100.75 103.714l72.482-.143c.043 39.398-32.284 71.434-72.16 71.434-39.878 0-72.204-32.036-72.204-71.554"
+                    fill="none"
+                    stroke="#fff"
+                  />
+                  <path
+                    d="M72 125.143h28.75"
+                    fill="#009100"
+                    fill-rule="evenodd"
+                    stroke="#fff"
+                  />
+                  <path
+                    d="M100.75 103.714l-71.908-.143c.026-39.638 32.352-71.674 72.23-71.674 39.876 0 72.203 32.036 72.203 71.554"
+                    fill="none"
+                    stroke="#fff"
+                  />
+                  <path
+                    d="M100.75 82.286h28.75"
+                    fill="#009100"
+                    fill-rule="evenodd"
+                    stroke="#fff"
+                  />
+                  <path
+                    d="M100.75 125.143h28.75"
+                    fill="#009100"
+                    fill-rule="evenodd"
+                    stroke="#fff"
+                  />
+                </g>
+              </svg>
+            </div>
+
+            <div class="navbar-right">
+              <div class="lang lang-en lang-navbar">
+                    <a href="/#about" onclick="collapseMenu()">About</a>
+                    <a href="/#education" onclick="collapseMenu()">Education</a>
+                    <a href="/#experience" onclick="collapseMenu()">Experience</a>
+                    <a href="/#contact" onclick="collapseMenu()">Contact</a>
+              </div>
+              <div class="lang lang-es lang-navbar">
+                    <a href="/#about" onclick="collapseMenu()">Sobre Mi</a>
+                    <a href="/#education" onclick="collapseMenu()">Educación</a>
+                    <a href="/#experience" onclick="collapseMenu()">Experiencia</a>
+                    <a href="/#contact" onclick="collapseMenu()">Contacto</a>
+              </div>
+              <select id="language-select" onfocus='this.size=2;' onblur='this.size=0;' onchange='this.size=1; this.blur();'>
+                <option value="en">English</option>
+                <option value="es">Español</option>
+              </select>
+              <label class="switch">
+                <input type="checkbox" checked onclick="toggleDarkMode()">
+                <span class="slider round"></span>
+              </label>
+            </div>
+        </div>
+            <div class="content">
             <div class="wrapper">
                 <div class="content-box">
                     <div class="content-inner">
@@ -23,10 +102,17 @@
                 </div>
             </div>
         </div>
-        <?php
-            include('footer.php');
-            echo "\n";
-            include('cookie_notice.html')
-        ?>
+        <!-- Cookie Notice -->
+        <div id="cookie-notice" class="cookie-notice">
+            <p class="lang lang-en">This website uses cookies to enhance your browsing experience. We use functional cookies for language detection, remembering your language preference, and your dark mode selection. No personal data is collected or shared. By continuing to browse the site, you agree to our use of these necessary cookies. For more information, read our <a href="/privacy-policy">Privacy Policy</a>.</p>
+            <p class="lang lang-es">Este sitio web utiliza cookies para mejorar su experiencia de navegación. Utilizamos cookies funcionales para la detección de idioma, recordar su preferencia de idioma y su selección de modo oscuro. No se recopilan ni comparten datos personales. Al continuar navegando por el sitio, usted acepta nuestro uso de estas cookies necesarias. Para obtener más información, lea nuestra <a href="/privacy-policy">Política de Privacidad</a>.</p>
+            <button onclick="hideCookieNotice()" class="cookie-button">Got it!</button>
+        </div>
+        <!-- Footer -->
+        <div class="footer banner">
+            <footer>
+                Copyright  &copy; 2024 Darren Stokes
+            </footer>
+        </div>
     </body>
 </html>
--- nginx/web_code/page_not_found.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<head>
-</head>
-    <body>
-        <h1>Hi</h1>
-</body>
-    </html>
