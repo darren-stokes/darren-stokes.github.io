88f8b7ff628741c8be3f02bdcd17f804176d53c6
Darren Stokes <darrenstokes2@hotmail.com>
2024-04-10 13:09:27 +0200

    Remove it all

--- nginx/web_code/scripts/javascript.js
+++ nginx/web_code/scripts/javascript.js
@@ -353,16 +353,8 @@ function technologyConveyorBelt(){
             if (x < -iconWidth) {
                 ctx.drawImage(loadedIcons[i], x + (iconWidth + iconPadding * 2) * loadedIcons.length, iconYPosition, iconWidth, iconHeight);
             } else if (x > canvas.width - iconWidth) {
-                if (darkMode === 'enabled') {
-                    ctx.shadowColor = 'rgba(255, 255, 255, 0.5)';
-                    ctx.shadowBlur = 10;
-                    ctx.shadowOffsetX = 0;
-                    ctx.shadowOffsetY = 0;
-                }
                 ctx.drawImage(loadedIcons[i], x - (iconWidth + iconPadding * 2) * loadedIcons.length, iconYPosition, iconWidth, iconHeight);
             }
-            // Restore the state to remove the shadow for next icon
-            ctx.restore();
         }
     
         requestAnimationFrame(draw);
